65581ce70c4585681c3bd6323849e6ae7f70cb31
aminhafsa245-oss <aminhafsa245@gmail.com>
2026-07-23 20:11:56 +0200

    Update README

--- src/API.js
+++ src/API.js
@@ -1,11 +1,11 @@
-export async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+const API_URL = "https://fakestoreapi.com/products";
 
+export async function getProducts() {
   const response = await fetch(API_URL);
 
   if (!response.ok) {
     throw new Error("Failed to fetch products");
   }
 
-  return response.json();
+  return await response.json();
 }
